8892be556e2f6e8485405a253441f3daf24b4fa4
Desheye <aarealaafiaboyz@gmail.com>
2024-02-14 16:57:14 +0100

    gen

--- 0x0E-SQL_more_queries/14-my-genres.sql
+++ 0x0E-SQL_more_queries/14-my-genres.sql
@@ -5,5 +5,5 @@ SELECT tv_genres.name
        ON tv_genres.id = tv_show_genres.genre_id
        	  INNER JOIN tv_shows
 	  ON tv_shows.id = tv_show_genres.show_id
-	  WHERE tv_shows.title = 'House of Dragons'
+	  WHERE tv_shows.title = 'Supernatural'
        ORDER BY tv_genres.name;
