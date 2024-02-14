7d30fa0ead998d4d35ba59bd85af4ab18f129d3d
Desheye <aarealaafiaboyz@gmail.com>
2024-02-14 17:00:07 +0100

    spell check error fixed

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,5 +5,5 @@ SELECT tv_genres.name
        ON tv_genres.id = tv_show_genres.genre_id
        	  INNER JOIN tv_shows
 	  ON tv_shows.id = tv_show_genres.show_id
-	  WHERE tv_shows.title = 'Supernatural'
+	  WHERE tv_shows.title = 'True Detective'
        ORDER BY tv_genres.name;
